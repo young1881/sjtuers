8b3a0adefa988bb70e1812b0473679d1500d681e
Steven Young <ywch2002@126.com>
2022-03-21 19:56:32 +0800

    add corona-virus part

--- index/views.py
+++ index/views.py
@@ -4,31 +4,31 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 def index_view(request):
     if request.method == 'GET':
-        # import requests
-        # import json
-        #
-        # # 微博热搜数据
-        # weibo_request = requests.get('https://tenapi.cn/resou/')
-        # weibo = json.loads(weibo_request.content)['list']
-        # for i in range(len(weibo)):
-        #     weibo[i]['name'] = str(i+1) + '  '+ weibo[i]['name']
-        #     if len(weibo[i]['name']) > 18:
-        #         weibo[i]['name'] = weibo[i]['name'][:16] + '...'
-        #
-        # # 知乎热搜数据
-        # zhihu_request = requests.get('https://tenapi.cn/zhihuresou/')
-        # zhihu = json.loads(zhihu_request.content)['list']
-        # for i in range(len(zhihu)):
-        #     zhihu[i]['query'] = str(i + 1) + '  ' + zhihu[i]['query']
-        #     if len(zhihu[i]['query']) > 22:
-        #         zhihu[i]['query'] = zhihu[i]['query'][:20] + '...'
-        #
-        # local = {
-        #     'weibo' : weibo[:5],
-        #     'zhihu' : zhihu[:5],
-        # }
 
-        return render(request, 'websites.html')
+        # 微博热搜数据
+        weibo = get_data('https://tenapi.cn/resou/')['list']
+        for i in range(len(weibo)):
+            weibo[i]['name'] = str(i+1) + '  '+ weibo[i]['name']
+            if len(weibo[i]['name']) > 18:
+                weibo[i]['name'] = weibo[i]['name'][:16] + '...'
+
+        # 知乎热搜数据
+        zhihu = get_data('https://tenapi.cn/zhihuresou/')['list']
+        for i in range(len(zhihu)):
+            zhihu[i]['query'] = str(i + 1) + '  ' + zhihu[i]['query']
+            if len(zhihu[i]['query']) > 22:
+                zhihu[i]['query'] = zhihu[i]['query'][:20] + '...'
+
+        #疫情数据
+        corona = get_data('https://lab.isaaclin.cn//nCoV/api/overall')['results'][0]
+
+        local = {
+            'weibo' : weibo[:5],
+            'zhihu' : zhihu[:5],
+            'corona': corona,
+        }
+
+        return render(request, 'websites.html', local)
 
     elif request.method == 'POST':
         search_msg = request.POST['search_msg']
@@ -37,3 +37,14 @@ def index_view(request):
         else:
             baidu = "http://www.baidu.com/s?word=" + search_msg
         return HttpResponseRedirect(baidu)
+
+def get_data(url):
+    import requests
+    import json
+
+    session = requests.Session()
+    session.trust_env = False
+    request = session.get(url)
+    data = json.loads(request.content)
+
+    return data
