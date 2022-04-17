79ce147cc9369560c276aa6d59bc0108e84f8619
Steven Young <ywch2002@126.com>
2022-04-17 22:17:01 +0800

    search-box update

--- static/js/hide.js
+++ static/js/hide.js
@@ -1,21 +1,32 @@
-var counter_flag=1;
-$(".counter").click(function () {
+$(function () {
+    // 初始化
+    var counter_flag=0;
+    $("#author").hide();
+
+    // 诗词悬停
+    $("#sentence").hover(function () {
+        $("#author").slideToggle(500);
+    });
+
+  // 简洁模式
+  $(".counter").click(function () {
     if (counter_flag===1){
         $(".counter").animate({top:"35%"},250);
-        $(".search-box").animate({top:"50%"},250);
-        $(".news").hide(100);
-        $(".weather").hide(100);
-        $(".tools").hide(100);
-        $(".icons").hide(100);
+        $(".search-box").animate({top:"45%"},250);
+        $(".news").hide(200);
+        $(".weather").hide(200);
+        $(".tools").hide(200);
+        $(".icons").hide(200);
         counter_flag=0;
     }
     else {
         $(".counter").animate({top:"15%"},250);
-        $(".search-box").animate({top:"30%"},250);
-        $(".news").show(100);
-        $(".weather").show(100);
-        $(".tools").show(100);
-        $(".icons").show(100);
+        $(".search-box").animate({top:"25%"},250);
+        $(".news").show(200);
+        $(".weather").show(200);
+        $(".tools").show(200);
+        $(".icons").show(200);
         counter_flag=1;
     }
+});
 });
