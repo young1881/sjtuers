306418e71850052843b965685546e573e029df48
wuwumo <Zhan_zh@sjtu.edu.cn>
2022-04-30 22:35:12 +0800

    Restrict permissions for non-logged-on users

--- static/js/windows.js
+++ static/js/windows.js
@@ -13,4 +13,35 @@ function closeCountdownDialog(){
 }
 
 
+var layer=document.createElement("div");
+layer.id="layer";
+function noPermissions(date){
+    var style={
+        background:"RGB(105,105,105,0.9)",
+        color:"#fff",
+        position:"absolute",
+        zIndex:"1000",
+        width:"500px",
+        height:"40px",
+        left:"30%",
+        top:"45%",
+        borderRadius:"20px",
+    }
+    for(var i in style)
+        layer.style[i]=style[i];
+
+    // 当找不到id为layer的控件时
+    if(document.getElementById("layer")==null){
+        // 在body中添加layer控件（layer在上面创建的）
+        document.body.appendChild(layer);
+        layer.innerHTML=date;
+        layer.style.textAlign="center";
+        layer.style.lineHeight="40px";
+        layer.style.letterSpacing="5px";
+        layer.style.fontWeight="border";
+        layer.style.fontSize="18px";
+        setTimeout("document.body.removeChild(layer)",2000)
+    }
+}
+
 
